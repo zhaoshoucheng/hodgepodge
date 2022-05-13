42f591016b14d9cacfab4672384b46f5562b42c7
shoucheng.zhao <shoucheng.zhao@lkcoffee.com>
2022-05-13 10:15:31 +0800

    提交etcd操作

--- etcd/main.go
+++ etcd/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"10.218.21.167:22379","10.218.21.196:22379","10.218.21.204:22379"},
+		Endpoints:   []string{"10.218.21.167:22379", "10.218.21.196:22379", "10.218.21.204:22379"},
 		DialTimeout: 10 * time.Second,
 	})
 	if err != nil {
@@ -19,17 +19,16 @@ func main() {
 	}
 	ctx, _ := context.WithTimeout(context.TODO(), time.Second)
 	//key := "/lkfe/test/filter"
-	//key := "/lkfe/test/filter/config/traffic_proxy_policy/_lfe_grayscale_luckyudubbodemo_test4"
-	key := "/lkfe/test/"
-	resp, err := cli.KV.Get(ctx,key, clientv3.WithPrefix(), clientv3.WithRev(0))
+	key := "/lkfe/test/discover/endpoints/luckyudubbodemo@lfe.upstream/172.22.2.172:8080"
+	//key := "/lkfe/test/"
+	resp, err := cli.KV.Get(ctx, key, clientv3.WithPrefix(), clientv3.WithRev(0))
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	//fmt.Println(resp)
 	for _, value := range resp.Kvs {
-		fmt.Println(string(value.Key))
+		fmt.Println(string(value.Value))
 	}
-
 
 }
