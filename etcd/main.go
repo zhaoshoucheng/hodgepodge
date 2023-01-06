515fb42e8aefcbf9daa26d1ed5fcd1a05bd4b23d
zhaoshoucheng <shoucheng.zhao@lkcoffee.com>
2023-01-06 16:42:29 +0800

    增加etcd读写工具

--- etcd/main.go
+++ etcd/main.go
@@ -2,40 +2,121 @@ package main
 
 import (
 	"context"
-	"encoding/json"
+	"errors"
+	"flag"
 	"fmt"
+	"github.com/spf13/viper"
 	"go.etcd.io/etcd/clientv3"
+	"log"
+	"net"
 	"time"
 )
 
 func main() {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"10.218.21.167:22379", "10.218.21.196:22379", "10.218.21.204:22379"},
-		DialTimeout: 10 * time.Second,
-	})
+	configFile := flag.String("c", "config", "config filename (extension in include)")
+	flag.Parse()
+	viper.SetConfigName(*configFile)
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
 	if err != nil {
-		// handle error!
-		fmt.Printf("connect to etcd failed, err:%v\n", err)
+		log.Fatalf("failed to read config: %v", err)
 		return
-	}
-	ctx, _ := context.WithTimeout(context.TODO(), time.Second)
-	//key := "/lkfe/test/filter"
-	//key := "/lkfe/test/discover/endpoints/"
-	key := "/lkfe/test/"
-	resp, err := cli.KV.Get(ctx, key, clientv3.WithPrefix(), clientv3.WithRev(0))
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	//fmt.Println(resp.Header.Revision)
-	//fmt.Println(resp)
-	for _, value := range resp.Kvs {
-		data := make(map[string]interface{})
-		json.Unmarshal(value.Value, &data)
-		if _, exists := data["state"]; !exists {
-			continue
-		}
-		fmt.Println(data["state"])
 	}
 
+	PrintlnAllValue("/openresty/", "itest")
+	return
+}
+
+func PrintlnAllKey(path string, env string) error {
+	resp, err := getValue(path, env)
+	if err != nil {
+		return err
+	}
+	for _, value := range resp.Kvs {
+		fmt.Println(string(value.Key))
+	}
+	return nil
+}
+
+func PrintlnAllValue(path string, env string) (values []string, err error) {
+	resp, err := getValue(path, env)
+	if err != nil {
+		return
+	}
+	for _, value := range resp.Kvs {
+		fmt.Println(string(value.Value))
+		values = append(values, string(value.Value))
+	}
+	return
+}
+
+func getValue(path string, env string) (*clientv3.GetResponse, error) {
+	cli, err := getEtcdCli(env)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("connect to etcd failed, err:%v\n", err))
+	}
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second)
+	resp, err := cli.KV.Get(ctx, path, clientv3.WithPrefix(), clientv3.WithRev(0))
+	if err != nil {
+		return nil, err
+	}
+	return resp, nil
+}
+
+func DeleteValue(path string, env string) error {
+	cli, err := getEtcdCli(env)
+	if err != nil {
+		return errors.New(fmt.Sprintf("connect to etcd failed, err:%v\n", err))
+	}
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second)
+	resp, err := cli.KV.Delete(ctx, path)
+	if err != nil {
+		return err
+	}
+	fmt.Println(resp.Deleted)
+	return nil
+}
+
+func PutValue(path string, env string, value string) error {
+	cli, err := getEtcdCli(env)
+	if err != nil {
+		return errors.New(fmt.Sprintf("connect to etcd failed, err:%v\n", err))
+	}
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second)
+	resp, err := cli.KV.Put(ctx, path, value)
+	if err != nil {
+		return err
+	}
+	_ = resp
+	return nil
+}
+func getEtcdCli(env string) (*clientv3.Client, error) {
+	endpoints := viper.GetStringSlice(env + ".endpoints")
+	if len(endpoints) == 0 {
+		return nil, errors.New("config err")
+	}
+	userName := viper.GetString(env + ".username")
+	passWord := viper.GetString(env + ".password")
+	return clientv3.New(clientv3.Config{
+		Endpoints:   endpoints,
+		DialTimeout: 10 * time.Second,
+		Username:    userName,
+		Password:    passWord,
+	})
+}
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return err.Error()
+	}
+	var str string
+	for _, value := range addrs {
+		if ipnet, ok := value.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				str = str + ipnet.IP.String()
+			}
+		}
+	}
+	return str
 }
