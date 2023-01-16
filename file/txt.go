6b4d9244429fbc25c3f5be67f29c4d7425c668f3
zhaoshoucheng <shoucheng.zhao@lkcoffee.com>
2023-01-16 18:05:54 +0800

    增加txt文件读取函数

--- file/txt.go
+++ file/txt.go
@@ -1,1 +1,20 @@
 package file
+
+import (
+	"bufio"
+	"os"
+)
+
+func GetResourceFromTxt(fileName string) (resp []string, err error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lineText := scanner.Text()
+		resp = append(resp, lineText)
+	}
+	return
+}
--- file/txt_test.go
+++ file/txt_test.go
@@ -1,1 +1,18 @@
 package file
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetResourceFromTxt(t *testing.T) {
+	file := "/Users/zhaoshoucheng/data/src/mysrc/hodgepodge/tools/cp_file/cp_file.txt"
+	lins, err := GetResourceFromTxt(file)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, value := range lins {
+		fmt.Println(value)
+	}
+}
